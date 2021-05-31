0d0feb619e6451ceeae1ae4a0946d3dfc22dc8cc
mohd_rozaini <mohd.rozaini@gmail.com>
2021-05-31 18:14:35 +0300

    Add shop, collection preview and collection item

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import { Route, Switch } from 'react-router';
 import Homepage from './pages/homepage/homepage.component';
-
+import ShopPage from './shop/shop.component';
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
