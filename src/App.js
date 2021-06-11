3541b765b4de8e2104194a8ff773fec2381e3e78
mohd_rozaini <mohd.rozaini@gmail.com>
2021-06-11 11:16:54 +0300

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './components/firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './components/firebase/firebase.utils';
 
 
 class App extends Component {
@@ -19,10 +19,21 @@ class App extends Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user })
-
-      console.log(user)
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth)
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+          console.log(this.state)
+        })
+      } else {
+        this.setState({ currentUser: userAuth })
+      }
     })
   }
 
--- src/components/firebase/firebase.utils.js
+++ src/components/firebase/firebase.utils.js
@@ -10,17 +10,40 @@ const config = {
     storageBucket: "ecommerce-582c3.appspot.com",
     messagingSenderId: "1074431382747",
     appId: "1:1074431382747:web:61698e91a6a2315ceba8fe"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(config);
+};
 
-  export const auth = firebase.auth()
-  export const firestore = firebase.firestore()
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return
 
-  const provider = new firebase.auth.GoogleAuthProvider()
-  provider.setCustomParameters({ prompt: 'select_account' })
-  export const signInWithGoogle = () => {
-      return auth.signInWithPopup(provider)
-  }
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+    const snapShot = await userRef.get() // const snapShot = await firestore.doc(`users/${userAuth.uid}`).get()
+    
+    
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth
+        const createdAt = new Date()
 
-  export default firebase
+        try {
+            await userRef.set({ displayName, email, createdAt, ...additionalData })
+        } catch (error) {
+            console.log('error creating user', error.message)
+        }
+    }
+    return userRef
+}
+  
+// Initialize Firebase
+firebase.initializeApp(config);
+
+export const auth = firebase.auth()
+export const firestore = firebase.firestore()
+
+const provider = new firebase.auth.GoogleAuthProvider()
+provider.setCustomParameters({ prompt: 'select_account' })
+export const signInWithGoogle = () => {
+    return auth.signInWithPopup(provider)
+}
+
+export default firebase
+
+
